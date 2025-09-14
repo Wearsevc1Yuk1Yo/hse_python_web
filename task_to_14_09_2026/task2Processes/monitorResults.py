cf1bb7f051377dfc61def43a69a204eda9ed9cba
Wearsevc1Yuk1Yo <bevgenia880@gmail.com>
2025-09-14 14:46:08 +0300

    some comments

--- task_to_14_09_2026/task2Processes/monitorResults.py
+++ task_to_14_09_2026/task2Processes/monitorResults.py
@@ -6,9 +6,14 @@ from constsTask2 import WEBSITES, CHECK_INTERVAL
 async def monitor():
     while True:
         tasks = [check(url) for url in WEBSITES]
+        # тут все результаты по сайтам
+
         results = await asyncio.gather(*tasks)
-        
+        # ждемммм потом вернем
+
         for url, (available, response_time, headers) in zip(WEBSITES, results):
             print(f"{url}: Available={available}, Response Time={response_time:.2f}s")
-        
+        # красиво выводим все
+
         await asyncio.sleep(CHECK_INTERVAL)
+        # ждеммм минутку спим
