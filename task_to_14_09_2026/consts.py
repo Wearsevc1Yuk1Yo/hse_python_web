74509e4a1b57dba247f8c7e6181c4afa9328c3f6
Wearsevc1Yuk1Yo <bevgenia880@gmail.com>
2025-09-14 14:28:24 +0300

    constans task2

---
+++ task_to_14_09_2026/consts.py
@@ -0,0 +1,8 @@
+# сайты, которые будем мониторить
+WEBSITES = [
+    "https://github.com", "https://stackoverflow.com",
+    "https://google.com", "https://youtube.com"
+]
+
+# интервал проверки
+CHECK_INTERVAL = 60
